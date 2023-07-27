6972f3192354f35bbdd7bbacd9af6c6e8ba549ce
SiddheshC13 <140595930+SiddheshC13@users.noreply.github.com>
2023-07-27 11:39:29 +0530

    Update new.tf

--- new.tf
+++ new.tf
@@ -6,3 +6,10 @@ resource "azurerm_resource_group" "example" {
   name     = "exam"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example1" {
+  name     = "exam33"
+  location = "West Europe"
+}
+
+
